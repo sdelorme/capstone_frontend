6cd93b88e136dc2f23fd7b1e5f5e6e34c3e112db
Seth Delorme <delorme.s.m@gmail.com>
2021-04-22 21:03:39 -0500

    add simple milestones index page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Milestones from "../views/MilestonesIndex.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/milestones",
+    name: "Milestones",
+    component: Milestones,
   },
   {
     path: "/about",
---
+++ src/views/MilestonesIndex.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for="milestone in milestones" :key="milestone.id">
+      <div>
+        <strong>{{ milestone.milestone_category }}</strong>
+        <br />
+        {{ milestone.description }}
+        <hr />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the Milestones Index Page!",
+      milestones: [],
+    };
+  },
+  created: function () {
+    axios.get("/api/milestones").then((response) => {
+      console.log("milestones index", response);
+      this.milestones = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
