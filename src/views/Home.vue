5ff34a2f5245fd1e6e4fecdfd8323b30bfea3ca8
Seth Delorme <delorme.s.m@gmail.com>
2021-04-22 21:10:13 -0500

    add child index to home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,30 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="child in children" :key="child.id">
+      Child: {{ child.name }}
+      <hr />
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to Your Home Page!",
+      children: [],
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("/api/children").then((response)=> {
+      console.log("children index", response);
+      this.children = response.data;
+    });
+  },
   methods: {},
 };
 </script>
