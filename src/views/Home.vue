45e98157edf4ef05425fa91d900de819ab897b81
Seth Delorme <delorme.s.m@gmail.com>
2021-05-02 15:31:41 -0500

    add dynamic if logged in to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <router-link v-bind:to="`/children`">Take me to my children</router-link>
+    <router-link v-if="$parent.isLoggedIn()" v-bind:to="`/children`">Take me to my children</router-link>
     <br />
     <router-link v-bind:to="`/milestones`">Take me to the CDC Milestones</router-link>
   </div>
