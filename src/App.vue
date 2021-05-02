50b3bcca9ec26abca349ae110d779c2a58a63aa1
Seth Delorme <delorme.s.m@gmail.com>
2021-05-02 17:17:20 -0500

    init with boostrap styling, stopped at Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,91 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-if="!isLoggedIn()" to="/signup">Signup</router-link>
-      |
-      <router-link v-if="!isLoggedIn()" to="/login">Login</router-link>
-      |
-      <router-link to="/">Home</router-link>
-      |
-      <router-link v-if="isLoggedIn()" to="/children">My Children</router-link>
-      |
-      <router-link to="/milestones">CDC Milestones</router-link>
-      |
-      <router-link v-if="isLoggedIn()" to="/children/new">Add Child</router-link>
-      |
-      <router-link v-if="isLoggedIn()" to="/logout">Logout</router-link>
+      <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+        <div class="container">
+          <a class="navbar-brand js-scroll-trigger" href="#page-top">Grow, Baby, Grow!</a>
+          <button
+            class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            Menu
+            <i class="fas fa-bars"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li v-if="!isLoggedIn()" class="nav-item mx-0 mx-lg-1">
+                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/signup">Sign Up</a>
+              </li>
+              <li v-if="!isLoggedIn()" class="nav-item mx-0 mx-lg-1">
+                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/login">Login</a>
+              </li>
+              <li class="nav-item mx-0 mx-lg-1">
+                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/">Home</a>
+              </li>
+              <li class="nav-item mx-0 mx-lg-1">
+                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/milestones">CDC Milestones</a>
+              </li>
+              <li v-if="isLoggedIn()" class="nav-item mx-0 mx-lg-1">
+                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/children">My Children</a>
+              </li>
+              <li v-if="isLoggedIn()" class="nav-item mx-0 mx-lg-1">
+                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/children/new">Add Child</a>
+              </li>
+              <li v-if="isLoggedIn()" class="nav-item mx-0 mx-lg-1">
+                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/logout">Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
     <router-view />
+    <footer class="footer text-center">
+      <div class="container">
+        <div class="row">
+          <!-- Footer Location-->
+          <div class="col-lg-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Location</h4>
+            <p class="lead mb-0">
+              2215 John Daniel Drive
+              <br />
+              Clark, MO 65243
+            </p>
+          </div>
+          <!-- Footer Social Icons-->
+          <div class="col-lg-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Around the Web</h4>
+            <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+          </div>
+          <!-- Footer About Text-->
+          <div class="col-lg-4">
+            <h4 class="text-uppercase mb-4">About Freelancer</h4>
+            <p class="lead mb-0">
+              Freelance is a free to use, MIT licensed Bootstrap theme created by
+              <a href="http://startbootstrap.com">Start Bootstrap</a>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <div class="scroll-to-top d-lg-none position-fixed">
+      <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
+        <i class="fa fa-chevron-up"></i>
+      </a>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 
 <script>
 export default {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <router-link v-if="$parent.isLoggedIn()" v-bind:to="`/children`">Take me to my children</router-link>
-    <br />
-    <router-link v-bind:to="`/milestones`">Take me to the CDC Milestones</router-link>
+    <header class="masthead bg-primary text-white text-center">
+      <div class="container d-flex align-items-center flex-column">
+        <!-- Masthead Avatar Image-->
+        <img class="masthead-avatar mb-5" src="/assets/img/avataaars.svg" alt="..." />
+        <!-- Masthead Heading-->
+        <h1 class="masthead-heading text-uppercase mb-0">{{ message }}</h1>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+          <div class="divider-custom-line"></div>
+          <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+          <div class="divider-custom-line"></div>
+        </div>
+        <!-- Masthead Subheading-->
+        <p class="masthead-subheading font-weight-light mb-0">
+          <a v-if="$parent.isLoggedIn()" href="/children">Take me to my children</a>
+        </p>
+        <p class="masthead-subheading font-weight-light mb-0">
+          <router-link v-bind:to="`/milestones`">Take me to the CDC Milestones</router-link>
+        </p>
+      </div>
+    </header>
+    <!-- THIS IS THE BREAK -->
   </div>
 </template>
 
@@ -13,7 +31,7 @@
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome parents!",
     };
   },
   created: function () {},
