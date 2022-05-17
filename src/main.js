27235ed38b467d8cf3918ee54ec71c43ca317c9c
Seth Delorme <delorme.s.m@gmail.com>
2022-05-16 19:52:30 -0500

    add auto not logged in detection to main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,21 @@ import axios from "axios";
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (401 === error.response.status) {
+      delete axios.defaults.headers.common["Authorization"];
+      localStorage.removeItem("jwt");
+      router.push("/login");
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
